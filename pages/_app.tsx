dc27a5683b2d670233a71b95cecdcdd608f84060
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-02-22 21:30:53 -0300

    removed all legacy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,13 @@ import { theme } from '../src/styles/theme'
 import { GlobalStyle } from '../src/styles/global-style'
 
 import { ThemeProvider } from 'styled-components'
-import { OptionCardProvider } from '../src/contexts/optionCardContext'
 
 export default function App({ Component, pageProps }) {
     return (
         <>
             <ThemeProvider theme={theme}>
-                <OptionCardProvider>
-                    <GlobalStyle />
-                    <Component {...pageProps} />
-                </OptionCardProvider>
+                <GlobalStyle />
+                <Component {...pageProps} />
             </ThemeProvider>
         </>
     )
